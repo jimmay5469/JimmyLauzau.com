507ebdb4da952753b4c428c7596df6778c20fa4f
jimmay5469 <jimmay5469@gmail.com>
2013-05-31 14:49:52 -0400

    moved styles to stylesheet, added navbar animation (for better or for worse)

--- public/js/mainApp.js
+++ public/js/mainApp.js
@@ -10,6 +10,24 @@ app.factory('dataFormatter', function() {
 	return dataFormatters;
 });
 
+app.animation('slide-down', function($rootScope) {
+  return {
+    setup: function(element) {
+      $(element).css({display: 'none'});
+    },
+    start: function(element, done, memo) {
+      $(element).slideDown(150, done);
+    }
+  };
+});
+app.animation('slide-up', function($rootScope) {
+  return {
+    start: function(element, done, memo) {
+      $(element).slideUp(150, done);
+    }
+  };
+});
+
 function navCtrl($scope, $location) {
 	$scope.location = $location;
 }
