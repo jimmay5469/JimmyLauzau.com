88ffee6c352f1cd5143c5d61f0168dc078cdc341
jimmay5469 <jimmay5469@gmail.com>
2013-08-02 02:51:23 +0000

    more file moving, app.js and package.json content updated

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 var express = require('express')
-  , routes = require('./routes/index.js')
   , api = require('./routes/api.js')
   , http = require('http')
   , path = require('path');
@@ -8,25 +7,19 @@ var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-app.use(express.favicon(__dirname + '/public/img/favicon.ico'));
+app.use(express.favicon(__dirname + '/public/root/img/favicon.ico'));
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+app.use("", express.static(path.join(__dirname, 'public/root/')));
+app.use("/apps", express.static(path.join(__dirname, 'public/apps/')));
 
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.layout.index);
-app.get('/home', routes.home.index);
-app.get('/apps', routes.apps.index);
-app.get('/connect', routes.connect.index);
-app.get('/biking', routes.biking.index);
 app.get('/api/bikeRides.json', api.bikeRides.list);
 
 http.createServer(app).listen(app.get('port'), function(){
