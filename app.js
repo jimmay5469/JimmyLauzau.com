85e7fed9211bfb40d39f701e665ae897a12e6fa0
jimmay5469 <jimmay5469@gmail.com>
2013-08-06 20:51:46 +0000

    trying zombie for seo

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var express = require('express')
   , myRidesApi = require('./routes/apps/myRides/api.js')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , Browser = require('zombie');
 
 var app = express();
+var browser = new Browser();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -21,10 +23,43 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/api/apps/myRides/bikeRides.json', myRidesApi.bikeRides.list);
-app.get('/', function(req, res) { res.sendfile(__dirname + '/public/root/index.html'); });
-app.get('/apps', function(req, res) { res.sendfile(__dirname + '/public/root/index.html'); });
-app.get('/connect', function(req, res) { res.sendfile(__dirname + '/public/root/index.html'); });
-app.get('/apps/myRides', function(req, res) { res.sendfile(__dirname + '/public/root/index.html'); });
+
+app.get('/', function(req, res) {
+  if(req.url.indexOf("_escaped_fragment_") != -1) {
+    browser.visit('http://localhost:' + app.get('port') + '/', function () {
+      res.send(browser.html('html'));
+    });
+  } else {
+    res.sendfile(__dirname + '/public/root/index.html');
+  }
+});
+app.get('/apps', function(req, res) {
+  if(req.url.indexOf("_escaped_fragment_") != -1) {
+    browser.visit('http://localhost:' + app.get('port') + '/apps', function () {
+      res.send(browser.html('html'));
+    });
+  } else {
+    res.sendfile(__dirname + '/public/root/index.html');
+  }
+});
+app.get('/connect', function(req, res) {
+  if(req.url.indexOf("_escaped_fragment_") != -1) {
+    browser.visit('http://localhost:' + app.get('port') + '/connect', function () {
+      res.send(browser.html('html'));
+    });
+  } else {
+    res.sendfile(__dirname + '/public/root/index.html');
+  }
+});
+app.get('/apps/myRides', function(req, res) {
+  if(req.url.indexOf("_escaped_fragment_") != -1) {
+    browser.visit('http://localhost:' + app.get('port') + '/apps/myRides', function () {
+      res.send(browser.html('html'));
+    });
+  } else {
+    res.sendfile(__dirname + '/public/root/index.html');
+  }
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
